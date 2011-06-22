9d063c65f9b652e71436df0f0de9249253c89554
Jazmin Schroeder <jazminschroeder@gmail.com>
2011-06-21 21:47:34 -0500

    Refactor specs

--- spec/authorize_net_reporting_spec.rb
+++ spec/authorize_net_reporting_spec.rb
@@ -13,76 +13,70 @@ describe AuthorizeNetReporting do
       lambda { AuthorizeNetReporting.new }.should raise_error(ArgumentError)
     end
   end
-  
-  describe "API URL in test mode" do 
-    subject {AuthorizeNetReporting.new(test_mode)}
-    it 'should be test url' do
-      subject.api_url.should eql(AuthorizeNetReporting::TEST_URL)
-    end
-  end
-  
   describe "API URL in live mode" do
     subject { AuthorizeNetReporting.new(live_mode) }
     it "should be live url" do
       subject.api_url.should eql(AuthorizeNetReporting::LIVE_URL)
     end
   end
+  
+  before(:each) do
+    @authorize_net_reporting = AuthorizeNetReporting.new(test_mode)
+  end  
+  
+  describe "API URL in test mode" do 
+    it 'should be test url' do
+      @authorize_net_reporting.api_url.should eql(AuthorizeNetReporting::TEST_URL)
+    end
+  end
+
   describe "settled_batch_list" do
-    subject { AuthorizeNetReporting.new(test_mode) }
     context "when there are not batches settled" do
       it "should raise Standard Error 'no records found'" do
-        lambda { subject.settled_batch_list }.should raise_error(StandardError)
+        lambda { @authorize_net_reporting.settled_batch_list }.should raise_error(StandardError)
       end
     end
     context "when there are settled batches" do
       it "should return batches" do
-        batches = subject.settled_batch_list({:first_settlement_date => "2011/04/20", :last_settlement_date => "2011/05/20"})
+        batches = @authorize_net_reporting.settled_batch_list({:first_settlement_date => "2011/04/20", :last_settlement_date => "2011/05/20"})
         batches.size.should eql(4)    
       end
     end
     context "when request include statistics" do
       it "should return statistis as an Array" do 
-        batches = subject.settled_batch_list({:first_settlement_date => "2011/04/20", :last_settlement_date => "2011/05/20", :include_statistics => true})
+        batches = @authorize_net_reporting.settled_batch_list({:first_settlement_date => "2011/04/20", :last_settlement_date => "2011/05/20", :include_statistics => true})
         batches.first.statistics.should be_an_instance_of(Array)
       end  
     end  
   end
   
   describe "batch_statistics" do
-    subject { AuthorizeNetReporting.new(test_mode) }
     it "should return an array statistics for given batch" do
-      subject.batch_statistics(1049686).statistics.should be_an_instance_of(Array)
+      @authorize_net_reporting.batch_statistics(1049686).statistics.should be_an_instance_of(Array)
     end
   end
   
   describe "transactions_list" do
-    subject {AuthorizeNetReporting.new(test_mode)}
     it "should return all transactions in a specified batch" do
-      transactions = subject.transaction_list(1049686)
+      transactions = @authorize_net_reporting.transaction_list(1049686)
       transactions.size.should eql(4)
     end
   end  
 
   describe "unsettled_transaction_list" do
-    subject { AuthorizeNetReporting.new(test_mode) }
     it "should return unsettled transactions" do
-      transactions = subject.unsettled_transaction_list
+      transactions = @authorize_net_reporting.unsettled_transaction_list
       transactions.should be_an_instance_of(Array)
     end  
   end  
   
-  
   describe "transaction_details" do
-    subject { AuthorizeNetReporting.new(test_mode) } 
     it "should return transaction if transaction_exists" do
-      transaction = subject.transaction_details(2157585857)
+      transaction = @authorize_net_reporting.transaction_details(2157585857)
       transaction.should be_an_instance_of(AuthorizeNetTransaction)
     end
     it "should raise StandardError 'record not found' if transaction doesn't exist" do
-      lambda { subject.transaction_details(0) }.should raise_error(StandardError)
+      lambda { @authorize_net_reporting.transaction_details(0) }.should raise_error(StandardError)
     end
   end
-  
-  
-  
 end
